935fc2ed7f5bcd7da5dee8c0373f952884ffb42d
xiaokai.zhan <zhanxiaokai@changba.com>
2017-02-23 10:28:44 +0800

    Initial Commit

---
+++ VideoToolboxEncoder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoToolboxEncoder
+//
+//  Created by apple on 2017/2/23.
+//  Copyright © 2017年 xiaokai.zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
